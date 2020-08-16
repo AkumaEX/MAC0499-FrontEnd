32249c97052bde0b66311d63ea8e99d3de7a55db
JULIO KENJI UEDA <akumaex@gmail.com>
2020-08-16 00:28:17 -0300

    trocando marcadores por círculos

--- lib/google_maps.dart
+++ lib/google_maps.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'dart:async';
@@ -14,10 +15,11 @@ class GoogleMaps extends StatefulWidget {
 class _GoogleMapsState extends State<GoogleMaps> {
   Location location = Location();
   Map clusterData;
-  Set<Marker> markers = Set<Marker>();
+  Set<Circle> markers = Set<Circle>();
   double defaultZoom = 15;
-  LatLng currentPosition = LatLng(-23.5677947, -46.7365914);
-  GoogleMapController _controller;
+  Future currentLocation;
+  LatLng coordinates;
+  GoogleMapController mapController;
   StreamSubscription<LocationData> locationSubscription;
 
   Future getLocation() async {
@@ -38,64 +40,112 @@ class _GoogleMapsState extends State<GoogleMaps> {
         return;
       }
     }
-    locationSubscription = location.onLocationChanged.listen((locationData) {
-      currentPosition = LatLng(locationData.latitude, locationData.longitude);
-    });
+    LocationData locationData = await location.getLocation();
+    return LatLng(locationData.latitude, locationData.longitude);
   }
 
-  Future<Map> getClusterData(LatLng currentPosition) async {
+  Future<Map> getClusterData(LatLng currentLocation) async {
     var url =
-        'http://104.155.175.253/ml/api?latitude=${currentPosition.latitude}&longitude=${currentPosition.longitude}';
+        'http://104.155.175.253/ml/api?latitude=${currentLocation.latitude}&longitude=${currentLocation.longitude}';
     http.Response response = await http.get(url);
     return json.decode(response.body);
   }
 
-  void addMarkers(List locations) {
-    locations.forEach((location) {
-      markers.add(Marker(
-          markerId: MarkerId('${location[2]}${location[3]}'),
-          infoWindow: InfoWindow(title: location[0], snippet: location[1]),
-          position: LatLng(location[2], location[3])),);
+  StreamSubscription<LocationData> startTracking() {
+    if (locationSubscription != null) {
+      locationSubscription.cancel();
+    }
+    return location.onLocationChanged.listen((locationData) {
+      coordinates = LatLng(locationData.latitude, locationData.longitude);
+      mapController.moveCamera(CameraUpdate.newLatLng(coordinates));
     });
+  }
+
+  Future showMarkers(LatLng coordinates, BuildContext context) async {
+    getClusterData(coordinates).then((value) {
+      value['geo'].forEach((location) {
+        markers.add(
+          Circle(
+            circleId: CircleId('${location[2]}${location[3]}'),
+            center: LatLng(location[2], location[3]),
+            radius: 30,
+            strokeWidth: 2,
+            strokeColor: Colors.red,
+            consumeTapEvents: true,
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) => Container(
+                        padding: EdgeInsets.all(10),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text('Momento do Roubo',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold)),
+                            Text('Data: ${location[0]}',
+                                style: TextStyle(fontSize: 20)),
+                            Text('Horário: ${location[1]}',
+                                style: TextStyle(fontSize: 20))
+                          ],
+                        ),
+                      ));
+            },
+          ),
+        );
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    currentLocation = getLocation();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: <Widget>[
-        GoogleMap(
-            initialCameraPosition:
-                CameraPosition(target: currentPosition, zoom: defaultZoom),
-            onMapCreated: (controller) => _controller = controller,
-            onCameraMoveStarted: () {
-              if (locationSubscription != null) {
-                locationSubscription.cancel();
-              }
-            },
-            onCameraMove: (position) => currentPosition = position.target,
-            onCameraIdle: () async {
-              clusterData = await getClusterData(currentPosition);
-              setState(() {
-                addMarkers(clusterData['geo']);
-              });
-            },
-            markers: markers),
-        Align(
-            alignment: Alignment.center,
-            child: Icon(Icons.place, size: 40, color: Colors.red)),
-        RaisedButton(
-            child: Icon(Icons.my_location),
-            onPressed: () {
-              setState(() {
-                if (locationSubscription == null) {
-                  getLocation();
-                } else {
-                  locationSubscription.resume();
-                  _controller.moveCamera(CameraUpdate.newLatLng(currentPosition));
-                }
-              });
-            })
-      ]),
+      body: FutureBuilder(
+        future: currentLocation,
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Align(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              );
+            default:
+              coordinates = snapshot.data;
+          }
+          return Stack(children: <Widget>[
+            GoogleMap(
+                zoomControlsEnabled: false,
+                initialCameraPosition:
+                    CameraPosition(target: coordinates, zoom: defaultZoom),
+                onMapCreated: (controller) => mapController = controller,
+                onCameraMoveStarted: () {
+                  if (locationSubscription != null) {
+                    locationSubscription.cancel();
+                  }
+                },
+                onCameraMove: (position) => coordinates = position.target,
+                onCameraIdle: () {
+                  setState(() {
+                    showMarkers(coordinates, context);
+                  });
+                },
+                circles: markers),
+            Align(
+                child: Icon(Icons.place, size: 40, color: Colors.red),
+                alignment: Alignment.center),
+          ]);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.my_location),
+          onPressed: () => locationSubscription = startTracking()),
     );
   }
 }
