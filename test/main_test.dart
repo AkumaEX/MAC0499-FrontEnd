f3c9824e203f207842759313e936fb3bc161d010
JULIO KENJI UEDA <akumaex@gmail.com>
2020-08-20 22:45:56 -0300

    testes

---
+++ test/main_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:e_roubo/main.dart';
+import 'package:e_roubo/google_maps.dart';
+
+void main() {
+  testWidgets('Floating Action Button', (WidgetTester tester) async {
+    await tester.runAsync(() async {
+      await tester.pumpWidget(MyApp());
+      await tester.pump();
+      expect(find.byType(Scaffold), findsOneWidget);
+      expect(find.byType(FloatingActionButton), findsOneWidget);
+      expect(find.byIcon(Icons.my_location), findsOneWidget);
+    });
+  });
+
+  testWidgets('Google Maps', (WidgetTester tester) async {
+    await tester.runAsync(() async {
+      await tester.pumpWidget(MyApp());
+      await tester.pump();
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      expect(find.byType(GoogleMaps), findsOneWidget);
+    });
+  });
+}
